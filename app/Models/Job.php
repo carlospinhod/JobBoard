c40d8538fbce96e02f787a3503c84723d079a3be
Carlos Pinho <carlospinhod@gmail.com>
2024-04-02 22:30:26 +0100

    EP12
    Pivot Tables and BelongsToMany Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -18,5 +18,10 @@ class Job extends Model
     {
         return $this->belongsTo(Employer::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 }
 
