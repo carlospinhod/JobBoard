4be77732529543c03e6f5dda7a45c35d4c26b4a3
Carlos Pinho <carlospinhod@gmail.com>
2024-04-09 22:36:38 +0100

    EP17
    Always Validate. Never Trust the User.

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Route::get('/jobs/{id}', function ($id) {
     return view('jobs.show', ['job' => $job]);
 });
 Route::post('/jobs', function () {
-    //validation..
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
