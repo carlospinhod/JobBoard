bcb5837e299e6a28fad46fd535eb2dc9bb258c71
Carlos Pinho <carlospinhod@gmail.com>
2024-04-26 02:09:08 +0100

    EP23
    6 Steps to Authorization Mastery

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,22 @@ Route::controller(JobController::class)->group(function () {
     Route::patch('/jobs/{job}', 'update');
     Route::delete('/jobs/{job}', 'destroy');
 });
+
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
+Route::resource('jobs', JobController::class)->except(['index', 'show']);
 */
-Route::resource('jobs', JobController::class);
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 /*
 Route::resource('jobs', JobController::class, [
@@ -29,7 +43,7 @@ Route::resource('jobs', JobController::class, [
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
 
